c719bc90a27a574e11ba23a3aad8f9e4c07aa3cc
John Griffiths <John.Griffiths@verint.com>
2020-12-01 11:26:53 +0000

    Try to flush logging

--- scripts/posh.ps1
+++ scripts/posh.ps1
@@ -1,3 +1,15 @@
-write-verbose "Step 1"
-write-verbose "Step 2"
-exit 0
+function log($this, $status=0) {
+  if($status -eq 1) {
+    write-error $this
+    [Console]::Out.Flush()
+    exit 1
+  }
+  else {
+    write-host $this
+    [Console]::Out.Flush()
+  }
+}
+
+log("This is step 1")
+log("This is step 2")
+log("This is an error", 1)
